297a30b80683099fc2a3e68991813bccc887bea5
wuguanghui <willwu618@gmail.com>
2018-06-07 15:14:35 +0800

    [ADD]private important thing

---
+++ src/com/will/SnowBoyFriend.java
@@ -0,0 +1,20 @@
+package com.will;
+
+/**
+ * ClassName:SnowBoyFriend
+ * Description:
+ *
+ * @author Will Wu
+ * @email willwu618@gmail.com
+ * @date 2018年06月07日
+ */
+public class SnowBoyFriend {
+    public static void main(String[] args) {
+        String java = "will";
+        if ("will".equals(java)) {
+            System.out.println("喜欢");
+        } else {
+            System.out.println("不喜欢");
+        }
+    }
+}
